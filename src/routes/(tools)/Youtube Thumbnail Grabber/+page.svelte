fd2d2fcc5a91a78931b9f38988fe7a37d8e75b29
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-07-02 22:08:09 +0530

    New heading added

--- src/routes/(tools)/Youtube Thumbnail Grabber/+page.svelte
+++ src/routes/(tools)/Youtube Thumbnail Grabber/+page.svelte
@@ -4,8 +4,8 @@
 
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
 	<!-- Add tool here -->
+	<h1>Hii</h1>
 </div>
 
 <style>
-
 </style>
